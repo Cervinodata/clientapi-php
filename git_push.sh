cd2ffce500c51a4492f6d5716a043f5c4bf923ff
jaccodrenth-freave <j.drenth@freave.com>
2026-05-22 16:40:26 +0200

    automated update

--- git_push.sh
+++ git_push.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-# ref: https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
-#
-# Usage example: /bin/sh ./git_push.sh wing328 openapi-petstore-perl "minor update" "gitlab.com"
 
 git_user_id=$1
 git_repo_id=$2
@@ -10,34 +7,26 @@ git_host=$4
 
 if [ "$git_host" = "" ]; then
     git_host="github.com"
-    echo "[INFO] No command line input provided. Set \$git_host to $git_host"
 fi
 
 if [ "$git_user_id" = "" ]; then
     git_user_id="cervinodata"
-    echo "[INFO] No command line input provided. Set \$git_user_id to $git_user_id"
 fi
 
 if [ "$git_repo_id" = "" ]; then
     git_repo_id="php"
-    echo "[INFO] No command line input provided. Set \$git_repo_id to $git_repo_id"
 fi
 
 if [ "$release_note" = "" ]; then
     release_note="Minor update"
-    echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
-# Initialize the local directory as a Git repository
 git init
 
-# Adds the files in the local repository and stages them for commit.
 git add .
 
-# Commits the tracked changes and prepares them to be pushed to a remote repository.
 git commit -m "$release_note"
 
-# Sets or updates the remote
 REMOTE_URL="git@${git_host}:${git_user_id}/${git_repo_id}.git"
 
 if git remote get-url origin >/dev/null 2>&1; then
@@ -46,8 +35,7 @@ else
     git remote add origin "$REMOTE_URL"
 fi
 
-git pull origin master
+echo "Git pushing to $REMOTE_URL"
 
-# Pushes (Forces) the changes in the local repository up to the remote repository
-echo "Git pushing to git@${git_host}:${git_user_id}/${git_repo_id}.git"
-git push origin master 2>&1 | grep -v 'To https'
+git push origin master
+
